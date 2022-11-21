9bf04df2d73024bbcc8360fdfe313e698128d1fd
fabricio <fabriciospfullstack@gmail.com>
2022-11-21 16:45:48 -0300

    feat: :sparkles: xata and routes configured

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,81 @@
-import express from 'express'
+import express, {Express, Request, Response} from 'express'
+import dotenv from 'dotenv'
 
-const app = express()
+import { getXataClient, Events } from './xata'
 
-app.get('/', (req, res) => {
-  res.status(200).json({
-    message: 'ok'
-  })
+dotenv.config()
+
+const app:Express = express()
+app.use(express.json())
+
+const port = process.env.PORT || 3000
+const xata = getXataClient()
+
+type EventsAppResponse<T> = { err: string } | T
+
+app.get('/events', async (req: Request, res: Response) => {
+  try {
+    const response = await xata.db.events.getAll()
+
+    res.status(200).json({
+      length: response.length,
+      response,
+    })
+    
+  } catch (error) {
+    console.error(error)
+  }
 })
 
-const PORT = 3000 || 5000
-app.listen(PORT, () => {
-  console.log('Server runing at 🔥 : http://localhost:3000')
+app.post('/events', async (req: Request<{}, {}, Events>, res: Response<EventsAppResponse<Events>>) => {
+  try {
+    const body = req.body
+
+    const response = await xata.db.events.create(body)
+    
+    return res.status(200).json(response)
+  } catch (err) {
+    console.error(err)
+    return res.status(500).json({ err: "Oops! 😭 Algo deu errado." })
+  }
+})
+
+app.put('/events/:id', async (req: Request<{ id: string }, {}, Events>, res: Response<EventsAppResponse<Events>>) => {
+  const event = req.body
+  const id = req.params.id
+
+  try {
+    
+    const updatedEvent = await xata.db.events.update(id, event) as Events
+
+    if(!updatedEvent) {
+      res.status(404).json({ err: "Hmm 🤔 Não conseguimos encontrar pelo que você estava procurando." })
+    } 
+
+    res.status(200).json(updatedEvent)
+
+  } catch (error) {
+    console.error(error)
+    return res.status(500).json({ err: "Eita! 🥲 Algo deu errado!" })
+  }
+})
+
+app.delete('/events/:id', async (req: Request<{ id: string }, {}, Events>, res: Response<EventsAppResponse<Events>>) => {
+  const id = req.params.id
+  try {
+    const deletedEvent = await xata.db.events.delete(id) as Events
+
+    if(!deletedEvent) {
+      return res.status(500).json({ err: "Hmm 🤔 Não conseguimos encontrar pelo que você estava procurando." })
+    }
+
+    return res.status(200).json(deletedEvent)
+
+  } catch (error) {
+    return res.status(500).json({ err: "Hmm 🤔 Algo deu errado." })
+  }
+})
+
+app.listen(port, () => {
+  console.log(`Server running 🔥: http://localhost:${port}`)
 })
